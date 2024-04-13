36fbcde7aa4ee51966490e0b735ffd43ab33aff4
smileydavisl <smileydavisl@gmail.com>
2024-04-13 13:42:20 -0600

    map appears on webpage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src' // Adjust the path if needed
+    }
+  },
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxInject: `import React from 'react'`,
+    // Specify JSX as the loader for .js files
+    jsx: 'react',
+  }
+});
+
+
