8021bb124d699d37f19487ea0714ed5db05f1f0b
Ethan Way <ethan.way@comcast.net>
2024-04-27 13:27:07 -0600

    google map

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Observations from './Observations';
 import Map from './Map';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Observations />
+    <div className='app-container'>
       <Map />
+      <div>
+        <Observations />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,42 +1,27 @@
-import { useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
 const Map = () => {
-  const [map, setMap] = useState(null);
-
-  useEffect(() => {
-    const leafletMap = L.map('map');
-    setMap(leafletMap);
-
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        leafletMap.setView([latitude, longitude], 13);
-        //L.marker([latitude, longitude]).addTo(leafletMap);
-
-      },
-      error => {
-        console.error('Error getting current location:', error);
-      }
-    );
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(leafletMap);
-
-    // Return a cleanup function to remove the map instance when the component unmounts
-    return () => {
-      leafletMap.remove();
-    };
-  }, []); // Ensure useEffect runs only once when the component mounts
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCbAVEkhkMf11mpQXOUFzmyhFCCo_fmu3M'
+  });
 
   return (
-    <div>
-      <div id="map" style={{ height: '400px' }}></div>
+    <div className='map-container'>
+      {isLoaded ? (
+        <GoogleMap
+          center={{
+            lat: 38.5449,
+            lng: -106.9329,
+          }}
+          zoom={13}
+          mapContainerStyle={{
+            width: '100%',
+            height: '100%'
+          }}
+        />
+      ) : null}
     </div>
   );
-};
+}
 
 export default Map;
-
